aabbc008fc0e7e7bbfce4c78d59fda148c9aa894
AL3M4N <HermesXVIII@gmail.com>
2022-07-28 22:45:09 -0500

    1

--- src/GeneradorVertebrados.java
+++ src/GeneradorVertebrados.java
@@ -3,7 +3,7 @@ public class GeneradorVertebrados {
         Vertebrado[] miVertebrados = new Vertebrado[4];
         miVertebrados[0] = new Vertebrado("Camello", "Mamifero");
         miVertebrados[1] = new Vertebrado("Oso", "Mamifero");
-        miVertebrados[2] = new Vertebrado("Caballo", "Mamifero");
+        
         return miVertebrados;
     }
 }
