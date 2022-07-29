3d68579254c843a21216620c5a1f7c856ed77e6e
AL3M4N <HermesXVIII@gmail.com>
2022-07-28 22:41:22 -0500

    Nuevos Animales

--- src/GeneradorVertebrados.java
+++ src/GeneradorVertebrados.java
@@ -2,6 +2,8 @@ public class GeneradorVertebrados {
     public static Vertebrado[] generarArrayVertebrados() {
         Vertebrado[] miVertebrados = new Vertebrado[4];
         miVertebrados[0] = new Vertebrado("Camello", "Mamifero");
+        miVertebrados[1] = new Vertebrado("Oso", "Mamifero");
+        miVertebrados[2] = new Vertebrado("Caballo", "Mamifero");
         return miVertebrados;
     }
 }
